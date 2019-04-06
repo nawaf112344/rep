153cda674d1a6e7f3e54ca6999dd25be45c2a9f1
nawaf112344 <47340062+nawaf112344@users.noreply.github.com>
2019-04-06 03:18:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,10 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const prefix = '-'
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
 var { Client } = require("discord.js");
-var client = new Discord.Client()
-var {token, messageID, roleName} = {
-    "token": "NTU4NTMxMjYwNzM0MjQyODM0.XKfcsw.QcgZpICpyIHs8-Yt22g49P90lNg",
+var client = new Client();
+var {messageID, roleName} = {
     "messageID": "563863570878824452",
     "roleName": "-Members,"
 };
-client.login(token).then(() => {
+client.login(process.env.BOT_TOKEN).then(() => {
     client.on("raw", async (event) => {
         if (event.t !== "MESSAGE_REACTION_ADD") return undefined;
         if (event.d.message_id !== messageID) return undefined;
